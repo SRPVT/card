cae40d81d0d0ad815a3b5569c99b04608577f0f5
SRPVT <habibi786123r@gmail.com>
2024-11-12 19:41:03 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const userID = "738748102311280681";
+const userID = "887557388700368896";
 const elements = {
 	statusBox: document.querySelector(".status"),
 	statusImage: document.getElementById("status-image"),
